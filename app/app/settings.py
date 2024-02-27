4af9a5aa08408bc0759661ca888a4b184c2f0953
erkamesen <erkamesen789@gmail.com>
2024-02-27 16:31:47 +0300

    docs: add sqlite for test

--- app/app/settings.py
+++ app/app/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
+        "TEST": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db_test.sqlite3"),
+        },
     }
 }
 
